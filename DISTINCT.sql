a1d2261ece0250742d6d9687e0d417c3a4c9f479
Harshita Kumari <harshita.23scse1180474@galgotiasuniversity.ac.in>
2025-01-21 02:05:15 +0530

    Create DISTINCT.sql

---
+++ DISTINCT.sql
@@ -0,0 +1,16 @@
+/* Write a query to find the unique origin locations */
+select Distinct origin
+from Flights;
+
+/*
+┌──────────┐
+│  Origin  │
+├──────────┤
+│ Mumbai   │
+│ London   │
+│ Ohio     │
+│ New York │
+│ Beijing  │
+│ Damascus │
+└──────────┘
+*/
