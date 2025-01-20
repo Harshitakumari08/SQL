002de1c9b4735663996a1c2ae78951094c77ca79
Harshita Kumari <harshita.23scse1180474@galgotiasuniversity.ac.in>
2025-01-20 15:11:34 +0530

    Update Inserting Data into a Table.sql

--- Inserting Data into a Table.sql
+++ Inserting Data into a Table.sql
@@ -3,3 +3,15 @@
 INSERT INTO employee (Employee_id, Employee_name, department)
 VALUES (4, 'Marcus Garcia', 'Product'),
        (5, 'Samantha Park', 'Hr');
+
+/* output
+┌─────────────┬────────────────┬────────────┐
+│ Employee_id │ Employee_Name  │ Department │
+├─────────────┼────────────────┼────────────┤
+│ 1           │ Kayla Thompson │ Sales      │
+│ 2           │ Ethan Chen     │ Operations │
+│ 3           │ Julia Lee      │ Hr         │
+│ 4           │ Marcus Garcia  │ Product    │
+│ 5           │ Samantha Park  │ Hr         │
+└─────────────┴────────────────┴────────────┘
+*/
