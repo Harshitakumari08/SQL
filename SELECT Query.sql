d6278d8c7dfe41db60fafdebb96ef09184c5109c
Harshita Kumari <harshita.23scse1180474@galgotiasuniversity.ac.in>
2025-01-21 02:04:10 +0530

    Create SELECT Query.sql

---
+++ SELECT Query.sql
@@ -0,0 +1,20 @@
+/* Write a query which does the following
+- Fetch the entry specifically from 2 columns - 'Passenger_name' and 'Gender' */
+select Passenger_name , Gender
+from Flights
+/*
+┌────────────────┬────────┐
+│ Passenger_name │ Gender │
+├────────────────┼────────┤
+│ Jackson        │ Male   │
+│ Riya           │ Female │
+│ Roy            │ Male   │
+│ Anthony        │ Male   │
+│ Salim          │ Male   │
+│ Dia            │ Female │
+│ Jackson        │ Male   │
+│ Dia            │ Female │
+│ Riya           │ Female │
+│ Betty          │ Female │
+└────────────────┴────────┘
+*/
