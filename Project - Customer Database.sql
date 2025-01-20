40dec8fda15da61e4b186eac163e6e3184071072
Harshita Kumari <harshita.23scse1180474@galgotiasuniversity.ac.in>
2025-01-21 01:56:32 +0530

    Update Project - Customer Database.sql

--- Project - Customer Database.sql
+++ Project - Customer Database.sql
@@ -18,3 +18,15 @@ VALUES(1, 'John Smith', 25,  '123 Main St'),
 (5, 'David Lee', 35,  '555 Park Ave');
 
 select* from customer;
+
+/* output
+┌────┬───────────────┬─────┬──────────────┐
+│ Id │     Name      │ Age │   address    │
+├────┼───────────────┼─────┼──────────────┤
+│ 1  │ John Smith    │ 25  │ 123 Main St  │
+│ 2  │ Sarah Johnson │ 30  │ 456 Broadway │
+│ 3  │ Michael Brown │ 45  │ 789 5th Ave  │
+│ 4  │ Jessica Davis │ 28  │ 321 Elm St   │
+│ 5  │ David Lee     │ 35  │ 555 Park Ave │
+└────┴───────────────┴─────┴──────────────┘
+*/
