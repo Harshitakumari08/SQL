a6de574f5783c717f3bd306f424764401eb25889
Harshita Kumari <harshita.23scse1180474@galgotiasuniversity.ac.in>
2025-01-20 15:15:46 +0530

    Create Alter table.sql

---
+++ Alter table.sql
@@ -0,0 +1,14 @@
+/* Write a query to add a column 'Designation' to the table and set 'Null' as the default value. Output the entire table.*/
+ALTER TABLE employee
+ADD COLUMN Designation TEXT default NULL;
+
+select * from employee;
+/* ouput:-
+┌─────────────┬────────────────┬────────────┬─────────────┐
+│ Employee_id │ Employee_Name  │ Department │ Designation │
+├─────────────┼────────────────┼────────────┼─────────────┤
+│ 1           │ Kayla Thompson │ Sales      │ NULL        │
+│ 2           │ Ethan Chen     │ Operations │ NULL        │
+│ 3           │ Julia Lee      │ Hr         │ NULL        │
+└─────────────┴────────────────┴────────────┴─────────────┘
+*/
