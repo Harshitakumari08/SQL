b5914ced503fde5a5e2685b6ffeb42c779f6f448
Harshita Kumari <harshita.23scse1180474@galgotiasuniversity.ac.in>
2025-01-20 15:07:02 +0530

    Create Introduction to statement.sql

---
+++ Introduction to statement.sql
@@ -0,0 +1,9 @@
+ CREATE TABLE table_name (
+       column_1 data_type,
+       column_2 data_type,
+       column_3 data_type 
+    );
+/*
+table_name refers to the name of the specific table on which a particular command or operation is being performed.
+(column_1 data_type, column_2 data_type, column_3 data_type) are parameters.
+/*
