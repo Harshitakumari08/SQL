1c47f7e630a666070f14da5942fe3e870aaf3a7f
Harshita Kumari <harshita.23scse1180474@galgotiasuniversity.ac.in>
2025-01-22 12:42:31 +0530

    Create MAX() and MIN().sql

---
+++ MAX() and MIN().sql
@@ -0,0 +1,17 @@
+/*Write a query to find the highest and lowest 'Hourly_pay' of the employees from the table 'employee'*/
+select MAX(Hourly_pay) as 'max_pay'
+from employee;
+select MIN(Hourly_pay) as 'min_pay'
+from employee;
+
+--OUTPUT
+┌─────────┐
+│ max_pay │
+├─────────┤
+│ 55      │
+└─────────┘
+┌─────────┐
+│ min_pay │
+├─────────┤
+│ 28      │
+└─────────┘
